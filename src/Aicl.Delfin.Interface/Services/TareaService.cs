78c081f88dbcd03839fc6bfc9057cd044c4e7b12
angel ignacio colmenares laguado <angel.ignacio.colmenares@gmail.com>
2012-11-15 09:09:01 -0500

    Tareas : programacion en la parte del servidor

---
+++ src/Aicl.Delfin.Interface/Services/TareaService.cs
@@ -0,0 +1,137 @@
+using System;
+using ServiceStack.Common;
+﻿using ServiceStack.ServiceHost;
+using Aicl.Delfin.Model.Types;
+using Aicl.Delfin.Model.Operations;
+using Aicl.Delfin.BusinessLogic;
+using ServiceStack.ServiceInterface;
+using ServiceStack.OrmLite;
+using Mono.Linq.Expressions;
+using System.Collections.Generic;
+using ServiceStack.Common.Web;
+
+
+namespace Aicl.Delfin.Interface
+{
+	[RequiresAuthenticateAttribute(ApplyTo.Get)]
+	[PermissionAttribute(ApplyTo.All,"Cliente.update")]
+	public class TareaService:AppRestService<Tarea>
+	{
+		public override object OnGet (Tarea request)
+		{
+			try{
+				var httpRequest= RequestContext.Get<IHttpRequest>();
+
+				return Factory.Execute(proxy=>{
+
+					long? totalCount=null;
+					var paginador= new Paginador(httpRequest);       
+
+					var visitor = ReadExtensions.CreateExpression<Tarea>();
+					var predicate = PredicateBuilder.True<Tarea>();
+
+					if(request.IdCliente.HasValue && request.IdCliente.Value!=default(int)){
+						predicate=q=>q.IdCliente== request.IdCliente.Value;
+					}
+
+					var qs= httpRequest.QueryString["Cumplida"];
+					bool cumplida;
+					if(bool.TryParse(qs,out cumplida)){
+						predicate= predicate.AndAlso(q=>q.Cumplida==cumplida);
+					}
+
+					visitor.Where(predicate).OrderByDescending(f=>f.Fecha);
+
+	                if(paginador.PageNumber.HasValue)
+	                {
+						visitor.Select(r=> Sql.Count(r.Id));
+						totalCount= proxy.Count(visitor);
+						visitor.Select();
+	                    int rows= paginador.PageSize.HasValue? paginador.PageSize.Value:BL.ResponsePageSize;
+	                    visitor.Limit(paginador.PageNumber.Value*rows, rows);
+	                }
+                	
+					return new  Response<Tarea>(){
+						TotalCount=totalCount,
+						Data=proxy.Get(visitor)
+                	};
+
+				});
+			}
+			catch(Exception e){
+				return HttpResponse.ErrorResult<Response<Tarea>>(e,"GetTareaError");
+			}
+		}
+
+		public override object OnPost (Tarea request)
+		{
+			try{
+				if( request.Tema.IsNullOrEmpty())
+					throw HttpError.NotFound(string.Format("Debe indicar el tema de la tarea"));
+
+				Factory.Execute(proxy=>{
+					proxy.Create(request);
+				});
+
+				List<Tarea> data = new List<Tarea>();
+				data.Add(request);
+			
+				return new Response<Tarea>(){
+					Data=data
+				};
+
+			}
+			catch(Exception e){
+				return HttpResponse.ErrorResult<Response<Tarea>>(e,"PostTareaError");
+			}
+		}
+
+
+		public override object OnPut (Tarea request)
+		{
+			try{
+				if( request.Tema.IsNullOrEmpty())
+					throw HttpError.NotFound(string.Format("Debe indicar el tema de la tarea"));
+
+				Factory.Execute(proxy=>{
+					proxy.Update(request);
+				});
+
+				List<Tarea> data = new List<Tarea>();
+				data.Add(request);
+			
+				return new Response<Tarea>(){
+					Data=data
+				};
+
+			}
+			catch(Exception e){
+				return HttpResponse.ErrorResult<Response<Tarea>>(e,"PutTareaError");
+			}
+		}
+
+
+		public override object OnDelete (Tarea request)
+		{
+			try{
+		
+				Factory.Execute(proxy=>{
+					proxy.Delete<Tarea>(q=>q.Id==request.Id);
+				});
+
+				List<Tarea> data = new List<Tarea>();
+				data.Add(request);
+			
+				return new Response<Tarea>(){
+					Data=data
+				};
+
+			}
+			catch(Exception e){
+				return HttpResponse.ErrorResult<Response<Tarea>>(e,"DeleteTareaError");
+			}
+		}
+
+	}
+}
+
---
+++ src/Aicl.Delfin.Model/Types/Tarea.cs
@@ -0,0 +1,39 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using ServiceStack.DataAnnotations;
+using ServiceStack.DesignPatterns.Model;
+using ServiceStack.ServiceHost;
+
+namespace Aicl.Delfin.Model.Types
+{
+
+	[RestService("/Tarea/create","post")]
+	[RestService("/Tarea/read","get")]
+	[RestService("/Tarea/update/{Id}","put")]
+	[RestService("/Tarea/destroy/{Id}","delete" )]
+	[JoinTo(typeof(Cliente),"IdCliente","Id", Order=0) ]
+	public class Tarea:IHasId<int>
+	{
+		public Tarea ()
+		{
+		}
+
+		[AutoIncrement]
+		public int Id { get; set; }
+
+		public int? IdCliente {get; set;}
+
+        [StringLength(128)]
+        [Required]
+		public string Tema {get;set;}
+
+		public bool Cumplida {get;set;}
+
+		public DateTime Fecha {get; set;}
+
+		[BelongsTo(typeof(Cliente),"Nombre")]
+		public string NombreCliente {get; set;}
+
+	}
+}
+
